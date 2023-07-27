8944a6bfb97a6d62e3b0d5437c0fc062c0f5cda4
Francesco Hayes <francesco.hayes@alowh.com>
2023-07-27 12:54:18 -0500

    images save

--- build/customer-logos/index.asset.php
+++ build/customer-logos/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '513e403b4aa1936d6382');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '52645d5e9a490851b65f');
--- build/customer-logos/index.js
+++ build/customer-logos/index.js
@@ -238,37 +238,13 @@ function save({
     className: "mx-auto max-w-7xl px-6 lg:px-8"
   }, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("div", {
     className: "mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-5"
-  }, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("img", {
-    className: "col-span-2 max-h-12 w-full object-contain lg:col-span-1",
-    src: "https://tailwindui.com/img/logos/158x48/transistor-logo-white.svg",
-    alt: "Transistor",
-    width: 158,
-    height: 48
-  }), (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("img", {
-    className: "col-span-2 max-h-12 w-full object-contain lg:col-span-1",
-    src: "https://tailwindui.com/img/logos/158x48/reform-logo-white.svg",
-    alt: "Reform",
-    width: 158,
-    height: 48
-  }), (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("img", {
-    className: "col-span-2 max-h-12 w-full object-contain lg:col-span-1",
-    src: "https://tailwindui.com/img/logos/158x48/tuple-logo-white.svg",
-    alt: "Tuple",
-    width: 158,
-    height: 48
-  }), (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("img", {
-    className: "col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1",
-    src: "https://tailwindui.com/img/logos/158x48/savvycal-logo-white.svg",
-    alt: "SavvyCal",
-    width: 158,
-    height: 48
-  }), (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("img", {
+  }, images.map((image, index) => (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("img", {
     className: "col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1",
-    src: "https://tailwindui.com/img/logos/158x48/statamic-logo-white.svg",
-    alt: "Statamic",
+    src: image.src,
+    alt: image.alt,
     width: 158,
     height: 48
-  }))));
+  })))));
 }
 
 /***/ }),
